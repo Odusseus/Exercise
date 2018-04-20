c533e6bc8bbacaeb5c220a2872da7c25bcda6301
Pascal Boittin <pboittin@onvz.nl>
2018-04-20 14:21:07 +0200

    Basic chapter 2

---
+++ MySitecore/MySitecore/Controllers/ComponentsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Web.Mvc;
+using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Controllers;
+using Sitecore.Mvc.Presentation;
+
+namespace MySitecore.Controllers
+{
+    public class ComponentsController : SitecoreController
+    {
+        // GET: Components
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult HeroSlider()
+        {
+            Item contentItem = null;
+            var database = Context.Database;
+
+            if (database != null)
+            {
+                if (!String.IsNullOrEmpty(RenderingContext.Current.Rendering.DataSource))
+                {
+                    try
+                    {
+                        contentItem = database.GetItem(new Sitecore.Data.ID(RenderingContext.Current.Rendering.DataSource));
+
+                    }
+                    catch (Exception ex)
+                    {
+                        string x = ex.Message;
+                    }
+                }
+            }
+            return View(contentItem);
+        }
+    }
+}
---
+++ MySitecore/MySitecore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MySitecore.MvcApplication" Language="C#" %>
